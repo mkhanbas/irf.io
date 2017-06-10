c03f60d943a6bf1bd48c911c56d506a7a3d67718
Irf Khan <mkhanbas@gmail.com>
2017-06-09 22:31:40 -0500

    basic styling complete
    styling of basic site is complete, may add scrolling effects later

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import '../node_modules/aos/dist/aos.css';
 import AOS from 'aos';
 
+function Body() {
+  AOS.init();
+  return(
+    <div className="theBody">
+      <Title />
+      <Img />
+      <Boxes />
+    </div>
+  );
+}
 
 function Title(){
-  AOS.init();
   return (
     <div className="heading" data-aos="fade-left">
       <span className = "firstName">Mohammad </span> Irfan Khan
@@ -18,11 +27,51 @@ function Img() {
     <div className="prof" data-aos="fade-right"> </div>
   );
 }
-function Body() {
+
+function Boxes() {
   return(
-    <div>
-      <Title />
-      <Img />
+    <div className="boxes">
+      <Box0 />
+      <Box1 />
+      <Box2 />
+      <Box3 />
+    </div>
+  );
+}
+
+function Box0() {
+  return(
+    <div className="meeting">  
+      <span className="iconPush">
+        <i className="fa fa-calendar fa-3x"> </i>
+      </span>
+      <span className="captionTagMeeting"> meeting </span>
+    </div>
+  );
+}
+function Box1() {
+  return(
+    <div className="resume" data-aos=""> 
+      <span className="iconPush">
+        <i className="fa fa-file-text fa-3x"> </i>
+      </span>
+      <span className="captionTagMeeting"> resume </span>
+    </div>
+  );
+}
+function Box2() {
+  return(
+    <div className="github" data-aos=""> 
+      <i className="fa fa-github fa-3x" aria-hidden="true"> </i>
+      <span className="captionTag"> github </span>
+    </div>
+  );
+}
+function Box3() {
+  return(
+    <div className="linkedIn" data-aos=""> 
+      <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"> </i>
+      <span className="captionTag"> linkedIn </span>
     </div>
   );
 }
